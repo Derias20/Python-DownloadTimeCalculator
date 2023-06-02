3da5838f55e9f218fcbd54141944c13add74a27d
Derias <introwamasakra@gmail.com>
2023-06-02 17:09:03 +0200

    some improvements

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,14 @@
-#made by Derias -> https://github.com/Derias20
+#created by derias => https://github.com/Derias20
+#This calculator is only accurate if your device downloads data with the speed of your network.
 getnetworkspeed = int(input("Network speed(Mbps)"))
 convertMbpstoMB = (getnetworkspeed/8)
 getFilesize = int(input("File size (MB)"))
 seconds = (getFilesize/convertMbpstoMB)
 minutes = (seconds/60)
 hours = (minutes/60)
-if minutes > 60:
-    print("Downloading should take about"+" "+ str(round(hours,1))+"hour/s") 
-else:
-    print("Downloading should take about"+" "+str(round(minutes))+"minutes")    
+if seconds < 60:
+    print("Downloading should take about"+" "+str(round(seconds))+"seconds") 
+elif minutes < 60:
+    print("Downloading should take about"+" "+str(round(minutes))+"minutes")
+elif minutes > 60:
+        print("Downloading should take about"+" "+str(round(hours,1))+"hours")    
